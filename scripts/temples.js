521fe7de2520fb53dd100703214870fa6e58802c
olubisiayantoye <bisikingson@gmail.com>
2024-11-25 16:38:39 +0100

    modified:   scripts/temples.js 	modified:   styles/temples.css 	modified:   temples.html

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,15 +1,13 @@
+// Set the current year and last modified date
 let d = new Date();
-document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
-document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
+document.getElementById("currentYear").innerHTML = `&copy; ${d.getFullYear()}`;
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
 
-const hambutton = document.querySelector('#hambutton');
+// Hamburger button functionality
+const hambutton = document.querySelector("#hambutton");
+const navmenu = document.querySelector("#navmenu");
 
-hambutton.addEventListener('click', () => {
-  document.querySelector('h1').classList.toggle('show');
-  document.querySelector('#navmenu').classList.toggle('show');
-  hambutton.classList.toggle('show');
+hambutton.addEventListener("click", () => {
+  navmenu.classList.toggle("show"); // Toggle menu visibility
+  hambutton.classList.toggle("show"); // Toggle button state
 });
-
-function toggleActive(element) {
-  element.classList.toggle("active");
-}
