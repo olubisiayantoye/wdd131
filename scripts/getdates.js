39eed8c848201dc06e1ec7d236ba885e30263b87
olubisiayantoye <bisikingson@gmail.com>
2024-12-05 11:57:54 +0100

    modified:   scripts/getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,16 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Display the current year in the footer
-    const yearElement = document.getElementById('currentYear');
-    const currentYear = new Date().getFullYear();
-    if (yearElement) {
-      yearElement.textContent = currentYear;
-    }
-  
-    // Update the last modified date
-    const lastModifiedElement = document.getElementById('lastModified');
-    const lastModifiedDate = new Date(document.lastModified);
-    if (lastModifiedElement) {
-      lastModifiedElement.textContent = `Last Modified: ${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
-    }
-  });
-  
+// Set the current year and last modified date
+let d = new Date();
+document.getElementById("currentYear").innerHTML = `&copy; ${d.getFullYear()}`;
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
